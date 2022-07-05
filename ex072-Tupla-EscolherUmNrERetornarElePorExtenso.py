c0ca7684a6ffd3b8411989db59d93f56c69b0ebc
João Maykon <joaomaykonm@gmail.com>
2022-07-05 09:27:47 -0300

    ex72

--- ex072-Tupla-EscolherUmNrERetornarElePorExtenso.py
+++ ex072-Tupla-EscolherUmNrERetornarElePorExtenso.py
@@ -1,5 +1,5 @@
 from num2words import num2words
-#n = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+n = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 resposta = ' '
 while resposta not in 'Nn':
     nr = int(input('Digite um número entre 0 e 20: '))
---
+++ ex72-EscolherNrE-RetornorPorExtenso.py
@@ -0,0 +1,13 @@
+cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis',
+        'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze',
+        'quinze', 'desesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+resp = ' '
+while resp not in 'Nn':
+        while True:
+                num = int(input('Digite um valor entre 0 e 20: '))
+                if 0 <= num <= 20:
+                        break;
+                print('Tente novamente.', end='')
+        print(f'Você digitou o número: {cont[num]}')
+        resp = str(input('Deseja continuar: (S/N)')).upper().strip()[0]
+print('Obrigado e volte sempre!')
